a2cbd7990e41d7653039c885545c2d72b2f4dade
Peter Schaer <peter.schaer@bve.be.ch>
2014-08-12 11:21:26 +0200

    Umbau mit DB-Abfrage der zu killenden Verbindungen und Config-File

--- killSDEConnections.py
+++ killSDEConnections.py
@@ -1,18 +1,53 @@
 # -*- coding: latin-1 -*-
+#~ killSDEConnections.py
 import sys
 import subprocess
+import cx_Oracle
+import ConfigParser
 
 #~ Variablen (übergeben von Kommandozeile)
-host = sys.argv[1]
-port = sys.argv[2]
-password = sys.argv[3]
+db = sys.argv[1].upper()
+pw = sys.argv[2]
+configFile = "sdeConfig.ini"
 
-#~ Konstanten
-sdeuser = "sde"
+def getConfiguration(filename):
+	config = ConfigParser.ConfigParser()
+	config.read(filename)
+	c = {
+		'oracledb' : config.get(db, 'oracledb'),
+		'sdehost' : config.get(db, 'sdehost'),
+		'sdeport' : config.get(db, 'sdeport'),
+		'sdeuser' : config.get(db, 'sdeuser'),
+		'ARCGISSERVERHOSTS' : config.get('GENERAL', 'ARCGISSERVERHOSTS').split(','),
+		'sdepw' : pw
+	}
+	return c
 
-#~ Liste mit Connections (sde_id aus sde.process_information)
-conns = [1,2]
-for c in conns:
-	cmd = "sdemon -o kill -i " + port + " -s " + host + " -u " + sdeuser + " -p " + password + " -N -t " + str(c)
-	subprocess.call(cmd)
-	print cmd
+def getConnectionsToKill(configuration):
+	result = []
+	inClause = "('" + "','".join(configuration['ARCGISSERVERHOSTS']) + "')"
+	sql = "select sde_id, owner, nodename, start_time from SDE.PROCESS_INFORMATION where START_TIME < (select sysdate - interval '1' day from dual) and nodename in " + inClause
+	print sql
+	connection = cx_Oracle.connect(configuration['sdeuser'] + "/" + configuration['sdepw'] + "@" + configuration['oracledb'])
+	cursor = connection.cursor()
+	cursor.execute(sql)
+	connections = cursor.fetchall()
+	cursor.close()
+	connection.close()
+	
+	print "SDE-Connections to kill:"
+	for c in connections:
+		print c[0]
+		result.append(c[0])
+	return result
+
+def killConnections(connections, configuration):
+	for c in connections:
+		cmd = "sdemon -o kill -i " + configuration['sdeport'] + " -s " + configuration['sdehost'] + " -u " + configuration['sdeuser'] + " -p " + configuration['sdepw'] + " -N -t " + str(c)
+		#~ subprocess.call(cmd)
+		print cmd
+	
+if __name__ == '__main__':
+	configuration = getConfiguration(configFile)
+	connections = getConnectionsToKill(configuration)
+	killConnections(connections, configuration)
